6c21ea28d98f490d832d05c19cbacab279c5df00
Timmy T <timmy120505@gmail.com>
2024-07-07 17:49:24 -0400

    file start

---
+++ project_euler_problems.py
@@ -0,0 +1,3 @@
+#The goal of this program is to complete the challenges on the Project Euler website.
+#All credit for the problems goes to the creators of Project Euler, these are just my solutions to the problems
+
