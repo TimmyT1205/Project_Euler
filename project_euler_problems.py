c7d88fc8437ed103b31af58993fc562ea9ec8ff2
Timmy T <timmy120505@gmail.com>
2024-07-07 17:56:05 -0400

    start of more formatting before actual attempts

--- project_euler_problems.py
+++ project_euler_problems.py
@@ -1,3 +1,10 @@
 #The goal of this program is to complete the challenges on the Project Euler website.
 #All credit for the problems goes to the creators of Project Euler, these are just my solutions to the problems
 
+#the way that I will be taking care of the problems is by using functions
+#I will code each problem into a function so that way it is easy to call a problem and find it's solution
+#they will generalized in order to tackle the challenge more wholistically.
+def pe_1():
+    return None
+
+pe_1()
